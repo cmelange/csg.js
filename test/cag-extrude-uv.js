fc24469281849d7b28096102cdf2ae16922657e7
Cedric Mélange <cmela@renson.be>
2019-12-28 12:16:04 +0100

    fix normal direction in case of extrusion in negative z direction

--- test/cag-extrude-uv.js
+++ test/cag-extrude-uv.js
@@ -1,10 +1,10 @@
-CSG = require('../csg.js')
+CSG = require('../csg.js');
 
 let shape = CSG.CAG.fromPoints([[[0,0],[0,1],[1,1],[1,0]],
-                                [[0.25,0.25],[0.25,0.75],[0.75,0.75],[0.75,0.25]]])
-let geomExtrude = shape.extrude({offset: [0,0,1], twiststeps: 2, twistangle: 1})
+                                [[0.25,0.25],[0.25,0.75],[0.75,0.75],[0.75,0.25]]]);
+shape = CSG.CAG.fromPoints([[1,0],[1,1],[0,1],[0,0]]);
+let geomExtrude = shape.extrude({offset: [0,0,-1], twiststeps: 1, twistangle: 0});
 
-let geomRevolve = shape.rotateExtrude()
-console.log(geomRevolve)
+let geomRevolve = shape.rotateExtrude();
 
 let geomNegativeExtrude = CSG.CAG.fromPoints([[[0,0],[0,1],[1,1],[1,0]]]).extrude({offset: [0,0,-1], twiststeps: 2, twistangle: 0});
